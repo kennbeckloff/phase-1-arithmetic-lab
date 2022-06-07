4e36bf398f2581f43c44b3b5ac7356350bc96fda
kennbeckloff <kennbeckloff688@gmail.com>
2022-06-07 18:52:19 +0300

    commit tree 9def34a44dc034fa184b06cfe36d414b8c438e52

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = (num1 * num2);
+const num3 = 54;
+const num4 = 5;
+const random = Math.floor(Math.random()) + 1
+const mod = num3 % num4;
+const max= Math.max(2,4,6,20)
